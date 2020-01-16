d4ab66022ab44ea4071bacb6b8106d1e101d299b
bozar42 <5583771+Bozar@users.noreply.github.com>
2020-01-16 20:14:37 +0800

    Assembler: Convert decimal to binary.

---
+++ 06/NoSymbol/assembler.py
@@ -0,0 +1,15 @@
+def main():
+    bin = []
+    convertToBinary(12, bin)
+    print(bin)
+
+
+# https://stackoverflow.com/questions/22746429/c-decimal-to-binary-converting
+def convertToBinary(decimal, binary):
+
+    if decimal//2 != 0:
+        convertToBinary(decimal//2, binary)
+    binary.append(decimal % 2)
+
+
+main()
