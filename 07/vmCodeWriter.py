95570d16da29a5be78bf355ae0dbc69f9e9bb0fe
bozar42 <5583771+Bozar@users.noreply.github.com>
2020-01-29 21:43:55 +0800

    Translate push command.

--- 07/vmCodeWriter.py
+++ 07/vmCodeWriter.py
@@ -1,2 +1,32 @@
 def translateVMCommand(text):
-    return text
+    stackPoint = '256'
+    asmCommand = []
+    asmCommand += _setStackPoint(stackPoint)
+
+    for t in text:
+        if t[0] == 'C_PUSH':
+            asmCommand += _push(t[1], t[2])
+
+    return asmCommand
+
+
+def _push(arg1, arg2):
+    if arg1 == 'constant':
+        return [
+            '@' + arg2,
+            'D=A',
+            '@SP',
+            'A=M',
+            'M=D',
+            '@SP',
+            'M=M+1',
+        ]
+
+
+def _setStackPoint(stackPoint):
+    return [
+        '@' + stackPoint,
+        'D=A',
+        '@SP',
+        'M=D',
+    ]
