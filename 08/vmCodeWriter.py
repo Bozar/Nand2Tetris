3f247e739de19544a530a0d5bb52c5300b0c18cd
bozar42 <5583771+Bozar@users.noreply.github.com>
2020-02-02 14:32:25 +0800

    Clean up code.

--- 08/vmCodeWriter.py
+++ 08/vmCodeWriter.py
@@ -7,8 +7,25 @@ def translateVMCommand(text, symbol):
     index = 0
     asmCommand = []
     asmCommand += _setStackPoint(stackPoint)
+    tmpOutput = ()
 
-    arithmeticFunction = {
+    for t in text:
+        if t[0] == 'C_PUSH':
+            asmCommand += vmPushPop.push(t[1], t[2], symbol)
+        elif t[0] == 'C_POP':
+            asmCommand += vmPushPop.pop(t[1], t[2], symbol)
+        elif t[0] == 'C_ARITHMETIC':
+            tmpOutput = _getArithmeticCode(t[1], index)
+            asmCommand += tmpOutput[0]
+            index = tmpOutput[1]
+
+    asmCommand += _setEndlessLoop()
+
+    return asmCommand
+
+
+def _getArithmeticCode(command, index):
+    command2function = {
         'add': vmArithmetic.add,
         'sub': vmArithmetic.sub,
         'neg': vmArithmetic.neg,
@@ -21,20 +38,10 @@ def translateVMCommand(text, symbol):
     }
     requireJump = ['eq', 'gt', 'lt']
 
-    for t in text:
-        if t[0] == 'C_PUSH':
-            asmCommand += vmPushPop.push(t[1], t[2], symbol)
-        elif t[0] == 'C_POP':
-            asmCommand += vmPushPop.pop(t[1], t[2], symbol)
-        elif t[0] == 'C_ARITHMETIC':
-            if t[1] in requireJump:
-                asmCommand += arithmeticFunction[t[1]](index)
-                index += 1
-            else:
-                asmCommand += arithmeticFunction[t[1]]()
-    asmCommand += _setEndlessLoop()
-
-    return asmCommand
+    if command in requireJump:
+        return (command2function[command](index), index + 1)
+    else:
+        return (command2function[command](), index)
 
 
 def _setEndlessLoop():
