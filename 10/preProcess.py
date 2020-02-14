ece8ef752b91bf3bedd39953e935761705355eea
bozar42 <5583771+Bozar@users.noreply.github.com>
2020-02-14 13:00:14 +0800

    Remove block comments.

--- 10/preProcess.py
+++ 10/preProcess.py
@@ -3,13 +3,21 @@ import re
 
 def formatText(input):
     output = []
+    isBlockComment = False
 
     for line in input:
         line = _removeLeadingSpace(line)
         line = _removeTrailingSpace(line)
         line = _removeExtraSpace(line)
         line = _removeComment(line)
+
         if _isEmpty(line):
+            continue
+        elif _isBlockCommentHead(line):
+            isBlockComment = True
+            continue
+        elif isBlockComment:
+            isBlockComment = not _isBlockCommentTail(line)
             continue
         else:
             output.append(line)
@@ -33,9 +41,24 @@ def _removeExtraSpace(input):
 
 
 def _removeComment(input):
-    comment = r'\s*//.*$'
-    return re.sub(comment, '', input)
+    comment1 = r'\s*//.*$'
+    comment2 = r'^/\*.*\*/$'
+
+    input = re.sub(comment1, '', input)
+    input = re.sub(comment2, '', input)
+
+    return input
 
 
 def _isEmpty(input):
     return input == ''
+
+
+def _isBlockCommentHead(input):
+    head = r'^/\*'
+    return re.search(head, input) != None
+
+
+def _isBlockCommentTail(input):
+    head = r'\*/$'
+    return re.search(head, input) != None
