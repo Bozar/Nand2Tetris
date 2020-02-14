82004ddae4eb1b74ec7945f21a43eadeec1dcfe6
bozar42 <5583771+Bozar@users.noreply.github.com>
2020-02-14 20:59:00 +0800

    Convert enum data tag to xml label.

--- 10/dataTag.py
+++ 10/dataTag.py
@@ -31,3 +31,37 @@ class keyWord(Enum):
     FALSE = auto(),
     NULL = auto(),
     THIS = auto(),
+
+
+def getXMLtag(enumTag):
+    enum2string = {
+        tokenType.KEYWORD: 'keyword',
+        tokenType.SYMBOL: 'symbol',
+        tokenType.IDENTIFIER: 'identifier',
+        tokenType.INT_CONST: 'integerConstant',
+        tokenType.STRING_CONST: 'stringConstant',
+
+        keyWord.CLASS: 'class',
+        keyWord.METHOD: 'method',
+        keyWord.FUNCTION: 'function',
+        keyWord.CONSTRUCTOR: 'constructor',
+        keyWord.INT: 'int',
+        keyWord.BOOLEAN: 'boolean',
+        keyWord.CHAR: 'char',
+        keyWord.VOID: 'void',
+        keyWord.VAR: 'var',
+        keyWord.STATIC: 'static',
+        keyWord.FIELD: 'field',
+        keyWord.LET: 'let',
+        keyWord.DO: 'do',
+        keyWord.IF: 'if',
+        keyWord.ELSE: 'else',
+        keyWord.WHILE: 'while',
+        keyWord.RETURN: 'return',
+        keyWord.TRUE: 'true',
+        keyWord.FALSE: 'false',
+        keyWord.NULL: 'null',
+        keyWord.THIS: 'this',
+    }
+
+    return enum2string[enumTag]
