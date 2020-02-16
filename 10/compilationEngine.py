f4b87686b753072977017a9a16b56e80f410f788
bozar42 <5583771+Bozar@users.noreply.github.com>
2020-02-16 14:48:41 +0800

    WIP: Implement compilationEngine.py.

--- 10/compilationEngine.py
+++ 10/compilationEngine.py
@@ -11,8 +11,48 @@ def compile(text):
     return output
 
 
+def _isLeftCurlyBracket(text):
+    return text == handleXML.writeLine(tokenType.SYMBOL, '{')
+
+
+def _isRightCurlyBracket(text):
+    return text == handleXML.writeLine(tokenType.SYMBOL, '}')
+
+
+def _isLeftRoundBracket(text):
+    return text == handleXML.writeLine(tokenType.SYMBOL, '(')
+
+
+def _isRightRoundBracket(text):
+    return text == handleXML.writeLine(tokenType.SYMBOL, ')')
+
+
 def _isSemiColon(text):
     return text == handleXML.writeLine(tokenType.SYMBOL, ';')
+
+
+def _isVarDec(text):
+    return text == handleXML.writeLine(tokenType.KEYWORD, 'var')
+
+
+def _isLetStatement(text):
+    return text == handleXML.writeLine(tokenType.KEYWORD, 'let')
+
+
+def _isIfStatement(text):
+    return text == handleXML.writeLine(tokenType.KEYWORD, 'if')
+
+
+def _isWhileStatement(text):
+    return text == handleXML.writeLine(tokenType.KEYWORD, 'while')
+
+
+def _isDoStatement(text):
+    return text == handleXML.writeLine(tokenType.KEYWORD, 'do')
+
+
+def _isReturnStatement(text):
+    return text == handleXML.writeLine(tokenType.KEYWORD, 'return')
 
 
 def _isClassVarDec(text):
@@ -25,6 +65,20 @@ def _isSubroutineDec(text):
     return text in keyWords
 
 
+def _compileVarDec(text, index):
+    indexStart = index
+    indexEnd = index
+    output = []
+
+    while not _isSemiColon(text[indexEnd]):
+        indexEnd += 1
+    indexEnd += 1
+    for i in range(indexStart, indexEnd):
+        output.append(text[i])
+
+    return (output, indexEnd)
+
+
 def _compileClassVarDec(text, index):
     labelStart = '<classVarDec>'
     labelEnd = '</classVarDec>'
@@ -32,24 +86,88 @@ def _compileClassVarDec(text, index):
     indexEnd = index
     output = [labelStart]
 
-    while not _isSemiColon(text[index]):
+    while not _isSemiColon(text[indexEnd]):
         indexEnd += 1
     indexEnd += 1
 
     for i in range(indexStart, indexEnd):
-        output.append(text[index])
+        output.append(text[i])
     output.append(labelEnd)
 
-    return (output, index)
+    return (output, indexEnd)
+
+
+def _compileParameterList(text, index):
+    indexStart = index
+    indexEnd = index
+    output = []
+
+    while not _isRightRoundBracket(text[indexEnd]):
+        indexEnd += 1
+    for i in range(indexStart, indexEnd):
+        output.append(text[i])
+
+    return (output, indexEnd)
 
 
 def _compileSubroutine(text, index):
-    return (text, index)
+    # curlyBracket
+    labelStart = '<subroutineDec>'
+    labelEnd = '</subroutineDec>'
+    output = [labelStart]
+    listIndex = ()
+
+    # From start to '('.
+    indexStart = index
+    indexEnd = index
+    while not _isLeftRoundBracket(text[indexEnd]):
+        indexEnd += 1
+    indexEnd += 1
+    for i in range(indexStart, indexEnd):
+        output.append(text[i])
+    output.append(labelEnd)
+
+    # Parameter list.
+    listIndex = _compileParameterList(text, indexEnd)
+    output += listIndex[0]
+    indexStart = listIndex[1]
+    indexEnd = listIndex[1]
+
+    # Add ')' & '{'.
+    while not _isLeftCurlyBracket(text[indexEnd]):
+        indexEnd += 1
+    indexEnd += 1
+    for i in range(indexStart, indexEnd):
+        output.append(text[i])
+
+    # Add varDec* & statements.
+    while not _isRightCurlyBracket(text[indexEnd]):
+        if _isVarDec(text[indexEnd]):
+            listIndex = _compileVarDec(text, indexEnd)
+            output += listIndex[0]
+            indexEnd = listIndex[1]
+        elif _isLetStatement(text[indexEnd]):
+            pass
+        elif _isIfStatement(text[indexEnd]):
+            pass
+        elif _isWhileStatement(text[indexEnd]):
+            pass
+        elif _isDoStatement(text[indexEnd]):
+            pass
+        elif _isReturnStatement(text[indexEnd]):
+            pass
+
+    output.append(text[indexEnd])
+    indexEnd += 1
+
+    return (output, indexEnd)
 
 
 def _compileClass(text):
     index = 1
-    output = ['<class>']
+    labelStart = '<class>'
+    labelEnd = '</class>'
+    output = [labelStart]
     content = ''
     listIndex = ()
 
@@ -67,6 +185,6 @@ def _compileClass(text):
             listIndex = _compileSubroutine(text, index)
             output += listIndex[0]
             index = listIndex[1]
-    output += [text[index], '</class>']
+    output += [text[index], labelEnd]
 
     return output
