4d4275f67382081f7df5846bcf408eaeb8e0ff85
bozar42 <5583771+Bozar@users.noreply.github.com>
2020-02-16 17:54:09 +0800

    WIP: Clean up code.

--- 10/compilationEngine.py
+++ 10/compilationEngine.py
@@ -1,190 +1,43 @@
-import re
-
-import dataTag
+import compileClassVarDec
+import compileSubroutineDec
 import handleXML
-from dataTag import tokenType
+import verifyXMLline
 
 
 def compile(text):
-    output = []
-    output += _compileClass(text)
-    return output
-
-
-def _isLeftCurlyBracket(text):
-    return text == handleXML.writeLine(tokenType.SYMBOL, '{')
-
-
-def _isRightCurlyBracket(text):
-    return text == handleXML.writeLine(tokenType.SYMBOL, '}')
-
-
-def _isLeftRoundBracket(text):
-    return text == handleXML.writeLine(tokenType.SYMBOL, '(')
-
-
-def _isRightRoundBracket(text):
-    return text == handleXML.writeLine(tokenType.SYMBOL, ')')
-
-
-def _isSemiColon(text):
-    return text == handleXML.writeLine(tokenType.SYMBOL, ';')
-
-
-def _isVarDec(text):
-    return text == handleXML.writeLine(tokenType.KEYWORD, 'var')
-
-
-def _isLetStatement(text):
-    return text == handleXML.writeLine(tokenType.KEYWORD, 'let')
-
-
-def _isIfStatement(text):
-    return text == handleXML.writeLine(tokenType.KEYWORD, 'if')
-
-
-def _isWhileStatement(text):
-    return text == handleXML.writeLine(tokenType.KEYWORD, 'while')
-
-
-def _isDoStatement(text):
-    return text == handleXML.writeLine(tokenType.KEYWORD, 'do')
-
-
-def _isReturnStatement(text):
-    return text == handleXML.writeLine(tokenType.KEYWORD, 'return')
-
-
-def _isClassVarDec(text):
-    keyWords = ['static', 'field']
-    return text in keyWords
-
-
-def _isSubroutineDec(text):
-    keyWords = ['constructor', 'function', 'method']
-    return text in keyWords
-
-
-def _compileVarDec(text, index):
-    indexStart = index
-    indexEnd = index
-    output = []
-
-    while not _isSemiColon(text[indexEnd]):
-        indexEnd += 1
-    indexEnd += 1
-    for i in range(indexStart, indexEnd):
-        output.append(text[i])
-
-    return (output, indexEnd)
-
-
-def _compileClassVarDec(text, index):
-    labelStart = '<classVarDec>'
-    labelEnd = '</classVarDec>'
-    indexStart = index
-    indexEnd = index
-    output = [labelStart]
-
-    while not _isSemiColon(text[indexEnd]):
-        indexEnd += 1
-    indexEnd += 1
-
-    for i in range(indexStart, indexEnd):
-        output.append(text[i])
-    output.append(labelEnd)
-
-    return (output, indexEnd)
-
-
-def _compileParameterList(text, index):
-    indexStart = index
-    indexEnd = index
-    output = []
-
-    while not _isRightRoundBracket(text[indexEnd]):
-        indexEnd += 1
-    for i in range(indexStart, indexEnd):
-        output.append(text[i])
-
-    return (output, indexEnd)
-
-
-def _compileSubroutine(text, index):
-    # curlyBracket
-    labelStart = '<subroutineDec>'
-    labelEnd = '</subroutineDec>'
-    output = [labelStart]
+    labelClass = handleXML.writeLabelPairs('class')
+    startIndex = 1
+    currentIndex = 1
     listIndex = ()
+    output = []
 
-    # From start to '('.
-    indexStart = index
-    indexEnd = index
-    while not _isLeftRoundBracket(text[indexEnd]):
-        indexEnd += 1
-    indexEnd += 1
-    for i in range(indexStart, indexEnd):
-        output.append(text[i])
-    output.append(labelEnd)
-
-    # Parameter list.
-    listIndex = _compileParameterList(text, indexEnd)
-    output += listIndex[0]
-    indexStart = listIndex[1]
-    indexEnd = listIndex[1]
-
-    # Add ')' & '{'.
-    while not _isLeftCurlyBracket(text[indexEnd]):
-        indexEnd += 1
-    indexEnd += 1
-    for i in range(indexStart, indexEnd):
+    # Add: <class>.
+    output.append(labelClass[0])
+    # Move pointer: 'class' className '{'.
+    while not verifyXMLline.isLeftCurlyBracket(text[currentIndex]):
+        currentIndex += 1
+    # Move pointer: classVarDec*.
+    currentIndex += 1
+    # Add: 'class' className '{'.
+    for i in range(startIndex, currentIndex):
         output.append(text[i])
 
-    # Add varDec* & statements.
-    while not _isRightCurlyBracket(text[indexEnd]):
-        if _isVarDec(text[indexEnd]):
-            listIndex = _compileVarDec(text, indexEnd)
+    # Add & move pointer: classVarDec* subroutineDec* '}'.
+    while not verifyXMLline.isRightCurlyBracket(text[currentIndex]):
+        # Add & move pointer: classVarDec*.
+        if verifyXMLline.isClassVarDec(text[currentIndex]):
+            listIndex = compileClassVarDec.compile2xml(text, currentIndex)
             output += listIndex[0]
-            indexEnd = listIndex[1]
-        elif _isLetStatement(text[indexEnd]):
-            pass
-        elif _isIfStatement(text[indexEnd]):
-            pass
-        elif _isWhileStatement(text[indexEnd]):
-            pass
-        elif _isDoStatement(text[indexEnd]):
-            pass
-        elif _isReturnStatement(text[indexEnd]):
-            pass
-
-    output.append(text[indexEnd])
-    indexEnd += 1
-
-    return (output, indexEnd)
-
-
-def _compileClass(text):
-    index = 1
-    labelStart = '<class>'
-    labelEnd = '</class>'
-    output = [labelStart]
-    content = ''
-    listIndex = ()
-
-    for i in range(index, index + 3):
-        output.append(text[i])
-    index += 3
-
-    while index < len(text) - 2:
-        content = handleXML.getContent(text[index])
-        if _isClassVarDec(content):
-            listIndex = _compileClassVarDec(text, index)
+            currentIndex = listIndex[1]
+        # Add & move pointer: subroutineDec*.
+        elif verifyXMLline.isSubroutineDec(text[currentIndex]):
+            listIndex = compileSubroutineDec.compile2xml(text, currentIndex)
             output += listIndex[0]
-            index = listIndex[1]
-        elif _isSubroutineDec(content):
-            listIndex = _compileSubroutine(text, index)
-            output += listIndex[0]
-            index = listIndex[1]
-    output += [text[index], labelEnd]
+            currentIndex = listIndex[1]
+
+    # Add: '}'.
+    output.append(text[i])
+    # Add: </class>.
+    output.append(labelClass[1])
 
     return output
---
+++ 10/verifyXMLline.py
@@ -0,0 +1,30 @@
+import dataTag
+import handleXML
+from dataTag import tokenType
+from dataTag import keyWord
+
+
+def isLeftCurlyBracket(text):
+    return text == handleXML.writeLine(tokenType.SYMBOL, '{')
+
+
+def isRightCurlyBracket(text):
+    return text == handleXML.writeLine(tokenType.SYMBOL, '}')
+
+
+def isClassVarDec(text):
+    _static = handleXML.writeLine(
+        tokenType.KEYWORD, dataTag.getXMLtag(keyWord.STATIC))
+    _field = handleXML.writeLine(
+        tokenType.KEYWORD, dataTag.getXMLtag(keyWord.FIELD))
+    return text in [_static, _field]
+
+
+def isSubroutineDec(text):
+    _constructor = handleXML.writeLine(
+        tokenType.KEYWORD, dataTag.getXMLtag(keyWord.CONSTRUCTOR))
+    _function = handleXML.writeLine(
+        tokenType.KEYWORD, dataTag.getXMLtag(keyWord.FUNCTION))
+    _method = handleXML.writeLine(
+        tokenType.KEYWORD, dataTag.getXMLtag(keyWord.METHOD))
+    return text in [_constructor, _function, _method]
