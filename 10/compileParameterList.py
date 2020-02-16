5f62f0947d19b43d1a687f2c1568af785ef1b50f
bozar42 <5583771+Bozar@users.noreply.github.com>
2020-02-16 19:34:18 +0800

    Implement compileParameterList.py.

--- 10/compileParameterList.py
+++ 10/compileParameterList.py
@@ -1,2 +1,24 @@
+import handleXML
+import verifyXMLline
+
+
 def compile2xml(text, index):
-    return ()
+    labelClass = handleXML.writeLabelPairs('parameterList')
+    startIndex = index
+    currentIndex = index
+    output = []
+
+    # Add: <parameterList>.
+    output.append(labelClass[0])
+
+    # Move pointer: ((type varName) ( ',' type varName)*)? ')'.
+    while not verifyXMLline.isRightRoundBracket(text[currentIndex]):
+        currentIndex += 1
+    # Add: ((type varName) ( ',' type varName)*)? ')'.
+    for i in range(startIndex, currentIndex):
+        output.append(text[i])
+
+    # Add: </parameterList>.
+    output.append(labelClass[1])
+
+    return (output, currentIndex)
